c41d184af328f258aa80b99a2641ed79cd042e78
Dan Diaczun <dandiacz@uschizog8640.global.publicisgroupe.net>
2019-06-19 20:01:00 -0500

    removing bg image

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,6 @@ import logo from '../assets/images/logo.svg';
 
 const Header = (props) => (
     <header id="header" className="alt">
-        <span className="logo"><img src={logo} alt="" /></span>
         <h1>Hi, I'm Dan</h1>
         <p>I'm a UX developer in Chicago</p>
     </header>
