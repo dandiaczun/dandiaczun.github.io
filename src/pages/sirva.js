9cc138150c6d03f23927b2c841cf579da9a329a4
Dan Diaczun <dandiacz@uschizog8640.global.publicisgroupe.net>
2019-06-21 15:42:40 -0500

    adding southwest images

--- src/pages/sirva.js
+++ src/pages/sirva.js
@@ -12,18 +12,18 @@ class Generic extends React.Component {
         <Helmet title="Dan Diaczun | Chicago UX Developer" />
         {/* <HeaderGeneric /> */}
         <header id="header">
-            <h1>Southwest Airlines</h1>
-            <p>Origin & Destination Landing Pages</p>
+            <h1>SIRVA</h1>
+            <p>Site Re-Design Wireframes</p>
         </header>
         <div id="main">
           <section id="content" className="main">
             <h2>Summary</h2>
-            <p>In mid-2018 Performics pitched a re-design and build of their origin and destination landing pages, which are generate roughly $100 million in revenue. These new pages were designed to address past issues that Southwest had with these pages, mainly poor conversions and bounce rates. My team had worked in tandem with Southwest's UX team to create several personas that better reflected their current customer who was most likely to book a flight through one of these pages and designed our page to incorporate Southwest's updated branding. A modular based template was then built which would then power approximately 8600 landing pages on Southwest.com.</p>
+            <p>SIRVA is a global moving and relocation service provider that focuses on corporate priorities and leverages technology and analytics to provide industry-leading solutions. In order to better serve their customers, SIRVA </p>
             <span className="image main"><img src={pic04} alt="" /></span>
             <h2>Results</h2>
-            <p>A mobile first approach was adopted which saw increases in performance, accessiblity and SEO rankings. The new pages also saw a 300% increase in mobile bookings in their first 2 months of being live and continue to be some of the highest performing landing pages on Southwest.com.</p>
+            <p></p>
             <h2>Role &amp; Technologies Used</h2>
-            <p>During this project I served as the lead front-end developer and assisted with strategy, user experience, wireframes &amp; prototypes. The page template was built using Bootstrap and Django, which was then hosted on a proprietary Python application and AWS. </p>
+            <p>I served as the lead user experience designer on this project and was responsible for strategy and wireframes, which were built using Axure.</p>
           </section>
         </div>
       </Layout>
