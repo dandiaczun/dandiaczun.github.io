33538c4b5287b9c7e6e6dadc08ab1a162466b465
Dan Diaczun <dandiacz@uschizog8640.global.publicisgroupe.net>
2019-06-25 21:20:16 -0500

    southwest and clairol portfolio stuff

--- src/pages/clairolpro.js
+++ src/pages/clairolpro.js
@@ -2,7 +2,8 @@ import React from 'react'
 import Helmet from 'react-helmet'
 
 import Layout from '../components/layout'
-import pic04 from '../assets/images/pic04.jpg'
+import clairol_desktop from '../assets/images/clairol-desktop.jpg'
+import clairol_wireframe from '../assets/images/clairol-wireframe.jpg'
 
 class Generic extends React.Component {
   render() {
@@ -16,8 +17,11 @@ class Generic extends React.Component {
         </header>
         <div id="main">
           <section id="content" className="main">
+          <h2>Summary</h2>
             <p>While at Performics we were approached by ClariolPro, an existing client, who were unhappy with their current website and felt that it didn't match the look and feel of a new product launching in the spring of 2019. With minimal assets, my team and I re-designed and developed a new product page for their site that reflected their current branding and was designed to support their goal of increasing conversions and educating customers.</p>
-            <span className="image main"><img src={pic04} alt="" /></span>
+            <span className="image main"><img src={clairol_wireframe} alt="" /></span>
+            <span className="image main"><img src={clairol_desktop} alt="" /></span>
+            <h2>Results</h2>
             <p>The re-designed page saw a 12.4% click-through rate vs. 4.8% for the existing product pages and drove 18% of all site conversions. It was the 3rd most viewed product page on clairolpro.com and saw a bounce rate that was 51% lower than the site average.</p>
             <h2>Role & Technologies Used</h2>
             <p>During this project I served as the lead front-end developer and assisted with designing wireframes using Axure & the initial whiteboard strategy sessions. The page was developed using Bootstrap, Django & jQuery, and hosted on a propriety Python application & AWS.</p>
--- src/pages/southwest.js
+++ src/pages/southwest.js
@@ -3,6 +3,7 @@ import Helmet from 'react-helmet'
 
 import Layout from '../components/layout'
 import southwest_desktop from '../assets/images/southwest-desktop.jpg'
+import southwest_wireframe from '../assets/images/southwest-wireframe.jpg'
 
 class Generic extends React.Component {
   render() {
@@ -18,7 +19,10 @@ class Generic extends React.Component {
           <section id="content" className="main">
             <h2>Summary</h2>
             <p>In mid-2018 Performics pitched a re-design and build of their origin and destination landing pages, which are generate roughly $100 million in revenue. These new pages were designed to address past issues that Southwest had with these pages, mainly poor conversions and bounce rates. My team had worked in tandem with Southwest's UX team to create several personas that better reflected their current customer who was most likely to book a flight through one of these pages and designed our page to incorporate Southwest's updated branding. A modular based template was then built which would then power approximately 8600 landing pages on Southwest.com.</p>
-            <span className="image main"><img src={southwest_desktop} alt="" /></span>
+            <span className="image main"><img src={southwest_desktop} alt="" width="800" height="auto" /></span>
+            <span className="image secondary"><img src={southwest_wireframe} alt="" width="800" height="auto" /></span>
+            {/*<span className="image secondary"><img src={southwest_mobile} alt="" width="800" height="auto" /></span>*/}
+
             <h2>Results</h2>
             <p>A mobile first approach was adopted which saw increases in performance, accessiblity and SEO rankings. The new pages also saw a 300% increase in mobile bookings in their first 2 months of being live and continue to be some of the highest performing landing pages on Southwest.com.</p>
             <h2>Role &amp; Technologies Used</h2>
