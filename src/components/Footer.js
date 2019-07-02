a5bd99cd6094b41c49f7b32bbd71314b3c2f91f8
Dan Diaczun <dandiacz@uschizog8640.global.publicisgroupe.net>
2019-07-02 10:58:18 -0500

    contact info copy updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,27 +7,23 @@ const Footer = (props) => (
     <footer id="footer">
         <section id="contact">
             <h2>Get In Touch</h2>
-            <p>Sed lorem ipsum dolor sit amet et nullam consequat feugiat consequat magna adipiscing tempus etiam dolore veroeros. eget dapibus mauris. Cras aliquet, nisl ut viverra sollicitudin, ligula erat egestas velit, vitae tincidunt odio.</p>
+            <p>Send me an email or connect with me via social media. References and additional work samples are also available by request</p>
         </section>
         <section>
             <h2>My Contact Info</h2>
             <dl className="alt">
-                <dt>Address</dt>
-                <dd>1234 Somewhere Road &bull; Nashville, TN 00000 &bull; USA</dd>
-                <dt>Phone</dt>
-                <dd>(000) 000-0000 x 0000</dd>
+                <dt>Location</dt>
+                <dd>Chicago, Illinois, USA</dd>
                 <dt>Email</dt>
-                <dd><a href="#">information@untitled.tld</a></dd>
+                <dd><a href="#">dnd912@gmail.com</a></dd>
             </dl>
             <ul className="icons">
-                <li><a href="#" className="icon fa-twitter alt"><span className="label">Twitter</span></a></li>
-                <li><a href="#" className="icon fa-facebook alt"><span className="label">Facebook</span></a></li>
-                <li><a href="#" className="icon fa-instagram alt"><span className="label">Instagram</span></a></li>
+                <li><a href="#" className="icon fa-github alt"><span className="label">LinkedIn</span></a></li>
                 <li><a href="#" className="icon fa-github alt"><span className="label">GitHub</span></a></li>
-                <li><a href="#" className="icon fa-dribbble alt"><span className="label">Dribbble</span></a></li>
+                <li><a href="#" className="icon fa-instagram alt"><span className="label">Instagram</span></a></li>
             </ul>
         </section>
-        <p className="copyright">&copy; Untitled. Design: <a href="https://html5up.net">HTML5 UP</a>.</p>
+        <p className="copyright">&copy; Dan Diaczun 2019 | Developed with <a href="https://www.gatsbyjs.org/">Gatsby</a> </p>
     </footer>
 )
 
