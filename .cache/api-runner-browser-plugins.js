f315003e2876516b9c3a9dc0ec81dbeac1e491b5
Dan Diaczun <dandiacz@uschizog8640.global.publicisgroupe.net>
2019-07-19 09:49:33 -0500

    whoops

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,4 @@
+module.exports = [{
+      plugin: require('/Users/dandiacz/sites/portfolio/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ .cache/async-requires.js
@@ -0,0 +1,9 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("/Users/dandiacz/sites/portfolio/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */)
+}
+
+exports.data = () => import(/* webpackChunkName: "pages-manifest" */ "/Users/dandiacz/sites/portfolio/.cache/data.json")
+
