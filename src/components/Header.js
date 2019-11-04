5dbabc7d1c7e3bf1576f193af66f2f09c3c3a306
Dan Diaczun <dandiaczun@c-67-162-4-255.hsd1.in.comcast.net>
2019-11-04 11:25:54 -0600

    updating header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = (props) => (
     <header id="header" className="alt">
         <h1>Hi, I'm Dan</h1>
-        <p>I'm a UX developer in Chicago</p>
+        <p>I'm a user interface developer in Chicago</p>
     </header>
 )
 
